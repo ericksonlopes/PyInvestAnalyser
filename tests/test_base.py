d0ae640b6c41ff934ad009449f0d81fa1b1219a5
Erickson Lopes <ofc.erickson@gmail.com>
2023-06-30 17:47:24 -0300

    TESTS: github workflow

--- tests/test_base.py
+++ tests/test_base.py
@@ -7,21 +7,5 @@ class BaseTestClass(TestCase):
     def setUp(self) -> None:
         self.logger = Logger(save_log=False).logger
 
-        self.stock_list = [
-            'B3SA3',
-            'BMGB4',
-
-        ]
-        self.ref_list = [
-            'BIME11',
-            'MXRF11',
-
-        ]
-
-        self.bdrs_list = [
-            'AAPL34',
-            'AMZO34'
-        ]
-
     def tearDown(self) -> None:
         pass
--- tests/test_extract_info_from_ref.py
+++ tests/test_extract_info_from_ref.py
@@ -1,27 +1,12 @@
-import concurrent.futures
-
 from src.services import ExtractInfoFromREF
 from tests.test_base import BaseTestClass
 
 
 class TestExtractInfoREF(BaseTestClass):
     def test_get_info_active(self):
-        result_actives = []
-
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = [executor.submit(ExtractInfoFromREF().get_info_active, active) for active in self.ref_list]
-
-            for future in concurrent.futures.as_completed(futures):
-                try:
-                    active = future.result()
-
-                    if isinstance(active, str):
-                        active = ExtractInfoFromREF().get_active_keys_indicators(active)
-
-                    result_actives.append(active)
-
-                except Exception:
-                    assert False
-
-                finally:
-                    assert True
+        try:
+            ExtractInfoFromREF().get_info_active("MXRF11")
+        except Exception:
+            assert False
+        finally:
+            assert True
