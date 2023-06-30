be346105fdff9a38cba04842e6ac0c8715888f6b
Erickson Lopes <ofc.erickson@gmail.com>
2023-06-30 18:24:42 -0300

    TESTS: return tests

--- tests/test_extract_info_from_bdr.py
+++ tests/test_extract_info_from_bdr.py
@@ -1,12 +1,27 @@
+import concurrent.futures
+
 from src.services import ExtractInfoFromBDR
 from tests.test_base import BaseTestClass
 
 
 class TestExtractInfoBDR(BaseTestClass):
     def test_get_info_active(self):
-        try:
-            ExtractInfoFromBDR().get_info_active("AAPL34")
-        except Exception:
-            assert False
-        finally:
-            assert True
+        result_actives = []
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(ExtractInfoFromBDR().get_info_active, active) for active in self.bdrs_list]
+
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    active = future.result()
+
+                    if isinstance(active, str):
+                        active = ExtractInfoFromBDR().get_active_keys_indicators(active)
+
+                    result_actives.append(active)
+
+                except Exception:
+                    assert False
+
+                finally:
+                    assert True
--- tests/test_extract_info_from_stock.py
+++ tests/test_extract_info_from_stock.py
@@ -1,12 +1,27 @@
+import concurrent.futures
+
 from src.services import ExtractInfoFromStock
 from tests.test_base import BaseTestClass
 
 
 class TestExtractInfoStock(BaseTestClass):
     def test_get_info_active(self):
-        try:
-            ExtractInfoFromStock().get_info_active("PETR4")
-        except Exception:
-            assert False
-        finally:
-            assert True
+        result_actives = []
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(ExtractInfoFromStock().get_info_active, active) for active in self.stock_list]
+
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    active = future.result()
+
+                    if isinstance(active, str):
+                        active = ExtractInfoFromStock().get_active_keys_indicators(active)
+
+                    result_actives.append(active)
+
+                except Exception:
+                    assert False
+
+                finally:
+                    assert True
